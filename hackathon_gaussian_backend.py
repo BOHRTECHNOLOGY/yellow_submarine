24300ffb3b2426b92e5a48bc57f7d886184c2f64
mstechly <michal.stechly@gmail.com>
2018-07-20 10:22:49 -0400

    Working version without sampling.

--- hackathon_gaussian_backend.py
+++ hackathon_gaussian_backend.py
@@ -1,4 +1,3 @@
-
 import strawberryfields as sf
 from strawberryfields.ops import *
 from strawberryfields.ops import Dgate, Rgate, Sgate, BSgate
@@ -8,24 +7,33 @@ from qmlt.numerical.regularizers import l2
 import pdb
 from collections import Counter
 import numpy as np
+
 # This time we want to keep the parameter small via regularization and monitor its evolution
 # By logging it into a file and plotting it
 my_init_params = [make_param(constant=0.1, name='squeeze_0', regularize=True, monitor=True),
                   make_param(constant=0.1, name='squeeze_1', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='squeeze_2', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='squeeze_3', regularize=True, monitor=True),
                   make_param(constant=0.1, name='displacement_0', regularize=True, monitor=True),
-                  make_param(constant=0.1, name='displacement_1', regularize=True, monitor=True)]
-                  # make_param(constant=0.1, name='rotation_0', regularize=True, monitor=True),
-                  # make_param(constant=0.1, name='rotation_1', regularize=True, monitor=True)]
+                  make_param(constant=0.1, name='displacement_1', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='displacement_2', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='displacement_3', regularize=True, monitor=True)]
+
+c = 0
+A = np.array([[c,-2,-10,1],
+              [-2,c,1,5],
+              [-10,1,c,-2],
+              [1,5,-2,c]])
 
 
-def digitize_xp(value):
-    if value > 0.1:
-        bit = 1
-    elif value <= -0.1:
-        bit = -1
-    else:
-        bit = 0
-    return bit
+# def digitize_xp(value):
+#     if value > 0.1:
+#         bit = 1
+#     elif value <= -0.1:
+#         bit = -1
+#     else:
+#         bit = 0
+#     return bit
 
 # def digitize_xp(value):
 #     if value > 0:
@@ -34,26 +42,41 @@ def digitize_xp(value):
 #         bit = -1
 #     return bit
 
+def digitize_xp(value):
+    return np.tanh(value)
+
 def get_bits_from_circuit(A, n_qmodes, params):
     eng, q = circuit(A, n_qmodes, params)
 
     state = eng.run("gaussian")
     # print(state.reduced_gaussian([0])[0], state.reduced_gaussian([1])[0])
     ###
-    
+
     mu_0 = state.reduced_gaussian([0])[0]
     cov_0 = state.reduced_gaussian([0])[1]
     mu_1 = state.reduced_gaussian([1])[0]
     cov_1 = state.reduced_gaussian([1])[1]
+    mu_2 = state.reduced_gaussian([2])[0]
+    cov_2 = state.reduced_gaussian([2])[1]
+    mu_3 = state.reduced_gaussian([3])[0]
+    cov_3 = state.reduced_gaussian([3])[1]
 
+    # qmode_0_gauss = np.random.multivariate_normal(mu_0, cov_0)
+    # qmode_1_gauss = np.random.multivariate_normal(mu_1, cov_1)
+    # qmode_2_gauss = np.random.multivariate_normal(mu_2, cov_2)
+    # qmode_3_gauss = np.random.multivariate_normal(mu_3, cov_3)
 
-    qmode_0_gauss = np.random.multivariate_normal(mu_0, cov_0)
-    qmode_1_gauss = np.random.multivariate_normal(mu_1, cov_1)
+    x0 = mu_0[0]
+    x1 = mu_1[0]
+    x2 = mu_2[0]
+    x3 = mu_3[0]
 
-    x0 = qmode_0_gauss[0]
-    p0 = qmode_0_gauss[1]
-    x1 = qmode_1_gauss[0]
-    p1 = qmode_1_gauss[1]
+    # x0 = qmode_0_gauss[0]
+    # p0 = qmode_0_gauss[1]
+    # x1 = qmode_1_gauss[0]
+    # p1 = qmode_1_gauss[1]
+    # x2 = qmode_2_gauss[0]
+    # x3 = qmode_3_gauss[0]
     ###
 
 
@@ -68,13 +91,14 @@ def get_bits_from_circuit(A, n_qmodes, params):
 
     ### 
     # eng.print_applied()
-    # print(x0, p0, x0, p1)
-    # pdb.set_trace()
+    # print(x0, x1, x2, x3)
+    
     bit_0 = digitize_xp(x0)
-    bit_1 = digitize_xp(p0)
-    bit_2 = digitize_xp(x1)
-    bit_3 = digitize_xp(p1)
+    bit_1 = digitize_xp(x1)
+    bit_2 = digitize_xp(x2)
+    bit_3 = digitize_xp(x3)
     bits = [bit_0, bit_1, bit_2, bit_3]
+    # pdb.set_trace()
     return bits    
 
 def circuit(A, n_qmodes, params):
@@ -85,75 +109,100 @@ def circuit(A, n_qmodes, params):
 
     X = np.vstack((X_top, X_bot))
 
-    c = 1
-    A = np.array([[c,-2,-10,1],
-                  [-2,c,1,5],
-                  [-10,1,c,-2],
-                  [1,5,-2,c]])
+    # c = 0
+    # A = np.array([[c,-2,-10,1],
+    #               [-2,c,1,5],
+    #               [-10,1,c,-2],
+    #               [1,5,-2,c]])
+    zeros_4 = np.zeros((n_qmodes,n_qmodes))
+    c_prim = 1
+    A_prim = np.vstack((np.hstack((zeros_4, A)), np.hstack((A, zeros_4)))) + np.eye(2*n_qmodes)*c_prim
 
     d = 0.05
-    Cov = np.linalg.inv(I - X@(d*A)) - I/2
+    # Cov = np.linalg.inv(I - X@(d*A)) - I/2
+    Cov = np.linalg.inv(I - X@(d*A_prim)) - I/2
 
     eng, q = sf.Engine(n_qmodes)
 
     with eng:
-        # Thermal(params[4]) | (q[0])
-        # Thermal(params[5]) | (q[1])
+        # beamsplitter array
+        Gaussian(Cov) | q
 
         Sgate(params[0]) | q[0]
         Sgate(params[1]) | q[1]
+        Sgate(params[2]) | q[2]
+        Sgate(params[3]) | q[3]
 
-        Dgate(params[2]) | q[0]
-        Dgate(params[3]) | q[1]
+        Dgate(params[4]) | q[0]
+        Dgate(params[5]) | q[1]
+        Dgate(params[6]) | q[2]
+        Dgate(params[7]) | q[3]
 
-        # BSgate(params[4], params[5]) | (q[0], q[1])
-
-        # Rgate(params[4])  | q[0]
-        # Rgate(params[5])  | q[1]
-
-        # beamsplitter array
-        # Gaussian(Cov) | q
-        # Thermal(-0.419) | (q[0])
-        # Thermal(-0.07803) | (q[1])
-        Rgate(0.4679) | (q[0])
-        BSgate(0.6547, 0) | (q[0], q[1])
-        Rgate(-0.4655) | (q[0])
-        Rgate(2.676) | (q[1])
-        Sgate(-0.1543, 3.142) | (q[0])
-        Sgate(-0.02828, 0) | (q[1])
-        BSgate(0.7233, 0) | (q[0], q[1])
-        Rgate(2.356) | (q[0])
-        Rgate(-0.7854) | (q[1])
-        # GaussianTransform(Cov) | q
-        # MeasureHD | q[0]
-        # MeasureHD | q[1]
+        # Rgate(3.064) | (q[0])
+        # BSgate(0.01046, 0) | (q[0], q[1])
+        # Rgate(-1.865) | (q[2])
+        # BSgate(5.591e-14, 0) | (q[2], q[3])
+        # Rgate(2.979) | (q[1])
+        # BSgate(1.571, 0) | (q[1], q[2])
+        # Rgate(1.253) | (q[0])
+        # BSgate(1.571, 0) | (q[0], q[1])
+        # Rgate(-1.991) | (q[0])
+        # Rgate(1.335) | (q[1])
+        # Rgate(-0.982) | (q[2])
+        # Rgate(2.216) | (q[3])
+        # BSgate(-8.002e-13, 0) | (q[2], q[3])
+        # Rgate(0.5906) | (q[2])
+        # BSgate(-0.01046, 0) | (q[1], q[2])
+        # Rgate(-0.1513) | (q[1])
+        # Sgate(-0.06962, 0) | (q[0])
+        # Sgate(-0.06714, 3.142) | (q[1])
+        # Sgate(-0.05428, 3.142) | (q[2])
+        # Sgate(-0.05349, 0) | (q[3])
+        # Rgate(-1.571) | (q[0])
+        # BSgate(1.234, 0) | (q[0], q[1])
+        # Rgate(-1.571) | (q[2])
+        # BSgate(0.3369, 0) | (q[2], q[3])
+        # Rgate(-1.571) | (q[1])
+        # BSgate(0.1581, 0) | (q[1], q[2])
+        # Rgate(-1.571) | (q[0])
+        # BSgate(1.119, 0) | (q[0], q[1])
+        # Rgate(-2.356) | (q[0])
+        # Rgate(0.7854) | (q[1])
+        # Rgate(-2.356) | (q[2])
+        # Rgate(-2.356) | (q[3])
+        # BSgate(-1.119, 0) | (q[2], q[3])
+        # Rgate(3.142) | (q[2])
+        # BSgate(-1.472, 0) | (q[1], q[2])
+        # Rgate(-3.142) | (q[1])
 
     return eng, q
 
 def calculate_cost_once(A, n_qmodes, bits):
     cost_value = 0
     penalty = 5
-    for i in range(n_qmodes*2):
-        for j in range(n_qmodes*2):
-            if i==j:
-                continue
-            if bits[i] != 0 and bits[j] != 0:
-                cost_value += 0.25 * A[i][j] * (bits[i] - bits[j])**2
-            else:
-                cost_value += penalty
+    for i in range(n_qmodes):
+        for j in range(n_qmodes):
+            # if i==j:
+            #     continue
+            # if bits[i] != 0 and bits[j] != 0:
+            #     cost_value += 0.25 * A[i][j] * (bits[i] - bits[j])**2
+            # else:
+            #     cost_value += penalty
+
+            cost_value += 0.25 * A[i][j] * (bits[i] - bits[j])**2
     return cost_value
 
 def circuit_with_cost_function(params):
-    n_qmodes = 2
-    c = 10
+    n_qmodes = 4
+    # c = 10
 
-    A = np.array([[c,1,0,0],
-                  [1,c,1,0],
-                  [0,1,c,1],
-                  [0,0,1,c]])
+    # A = np.array([[c,1,0,0],
+    #               [1,c,1,0],
+    #               [0,1,c,1],
+    #               [0,0,1,c]])
 
     cost_value = 0
-    trials = 10
+    trials = 1
     for i in range(trials):
         bits = get_bits_from_circuit(A, n_qmodes, params)
         cost_value += calculate_cost_once(A, n_qmodes, bits)
@@ -190,9 +239,9 @@ def main():
                  'loss': myloss,
                  'regularizer': myregularizer,
                  'regularization_strength': 0.5,
-                 # 'optimizer': 'Nelder-Mead',
                  'optimizer': 'SGD',
-                 'init_learning_rate': 1e-7,
+                 # 'init_learning_rate': 1e-7,
+                 'init_learning_rate': 1e-1,
                  'log_every': 1,
                  'plot': True
                  }
@@ -200,7 +249,7 @@ def main():
 
   learner = CircuitLearner(hyperparams=hyperparams)
 
-  learner.train_circuit(steps=50)
+  learner.train_circuit(steps=100)
 
   # Print out the final parameters
   final_params = learner.get_circuit_parameters()
@@ -208,24 +257,23 @@ def main():
   for name, value in final_params.items():
       print("Parameter {} has the final value {}.".format(name, value))
 
-  c = 10
-  A = np.array([[c,1,0,0],
-                [1,c,1,0],
-                [0,1,c,1],
-                [0,0,1,c]])
 
-  n_qmodes = 2
+  n_qmodes = 4
   all_results = []
 
   final_params_translated = []
   final_params_translated.append(final_params["regularized/squeeze_0"])
   final_params_translated.append(final_params["regularized/squeeze_1"])
+  final_params_translated.append(final_params["regularized/squeeze_2"])
+  final_params_translated.append(final_params["regularized/squeeze_3"])
   final_params_translated.append(final_params["regularized/displacement_0"])
   final_params_translated.append(final_params["regularized/displacement_1"])
+  final_params_translated.append(final_params["regularized/displacement_2"])
+  final_params_translated.append(final_params["regularized/displacement_3"])
   # final_params_translated.append(final_params["regularized/rotation_0"])
   # final_params_translated.append(final_params["regularized/rotation_1"])
 
-  for i in range(1000):
+  for i in range(1):
       bits = get_bits_from_circuit(A, n_qmodes, final_params_translated)
       string_bits = [str(bit) for bit in bits]
       all_results.append(",".join(string_bits))
@@ -238,4 +286,10 @@ def main():
   # during training. Play around with the 'regularization_strength' and see how a large strength forces alpha to zero.
 
 if __name__ == '__main__':
+    # for b1 in [-1, 1]:
+    #    for b2 in [-1, 1]:
+    #       for b3 in [-1, 1]:
+    #           for b4 in [-1, 1]:
+    #               bits = [b1,b2,b3,b4]
+    #               print(bits, calculate_cost_once(A, 4, bits))
     main()
