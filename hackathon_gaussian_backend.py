b0d130396f19b99618c9f7a7fb8c8acd85cc5da0
mstechly <michal.stechly@gmail.com>
2018-07-19 21:04:43 -0400

    Added Displacement gates.

--- hackathon_gaussian_backend.py
+++ hackathon_gaussian_backend.py
@@ -6,17 +6,16 @@ from qmlt.numerical import CircuitLearner
 from qmlt.numerical.helpers import make_param
 from qmlt.numerical.regularizers import l2
 import pdb
+from collections import Counter
 import numpy as np
 # This time we want to keep the parameter small via regularization and monitor its evolution
 # By logging it into a file and plotting it
 my_init_params = [make_param(constant=0.1, name='squeeze_0', regularize=True, monitor=True),
-                  make_param(constant=-0.1, name='squeeze_1', regularize=True, monitor=True),
-                  # make_param(constant=0.3, name='squeeze_2', regularize=True, monitor=True),
-                  # make_param(constant=0.3, name='squeeze_3', regularize=True, monitor=True),
-                  make_param(constant=0.1, name='alpha_0', regularize=True, monitor=True),
-                  make_param(constant=-0.1, name='alpha_1', regularize=True, monitor=True)]
-                  # make_param(constant=0.05, name='alpha_2', regularize=True, monitor=True),
-                  # make_param(constant=0.01, name='alpha_3', regularize=True, monitor=True)]
+                  make_param(constant=0.1, name='squeeze_1', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='move_0', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='move_1', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='rotation_0', regularize=True, monitor=True),
+                  make_param(constant=0.1, name='rotation_1', regularize=True, monitor=True)]
 
 
 def digitize_xp(value):
@@ -25,6 +24,25 @@ def digitize_xp(value):
     elif value <= 0:
         bit = -1
     return bit
+
+def get_bits_from_circuit(A, n_qmodes, params):
+    eng, q = circuit(A, n_qmodes, params)
+
+    state = eng.run("gaussian")
+
+    val_0 = q[0].val
+    val_1 = q[1].val
+    x0 = np.real(val_0)
+    p0 = np.imag(val_0)
+    x1 = np.real(val_1)
+    p1 = np.imag(val_1)
+
+    bit_0 = digitize_xp(x0)
+    bit_1 = digitize_xp(p0)
+    bit_2 = digitize_xp(x1)
+    bit_3 = digitize_xp(p1)
+    bits = [bit_0, bit_1, bit_2, bit_3]
+    return bits    
 
 def circuit(A, n_qmodes, params):
     I = np.eye(2*n_qmodes)
@@ -42,12 +60,15 @@ def circuit(A, n_qmodes, params):
 
     with eng:
         # S = Sgate(params[0])
-        Sgate(np.clip(params[0], -1, 1)) | q[0]
-        Sgate(np.clip(params[1], -1, 1)) | q[1]
+        Sgate(params[0]) | q[0]
+        Sgate(params[1]) | q[1]
+
+        Dgate(params[2]) | q[0]
+        Dgate(params[3]) | q[1]
 
         # rotation gates
-        Rgate(params[2])  | q[0]
-        Rgate(params[3])  | q[1]
+        Rgate(params[4])  | q[0]
+        Rgate(params[5])  | q[1]
 
         # beamsplitter array
         Gaussian(A) | q
@@ -56,46 +77,33 @@ def circuit(A, n_qmodes, params):
 
     return eng, q
 
-def circuit_with_cost_function(params):
-    n_qmodes = 2
-    backend = "gaussian"
-    c = 10
-    optimization_dim = 3
-
-    A = np.array([[c,1,0,0],
-              [1,c,1,0],
-              [0,1,c,1],
-              [0,0,1,c]])
-
-    eng, q = circuit(A, n_qmodes, params)
-
-    state = eng.run(backend)
-
-    val_0 = q[0].val
-    val_1 = q[1].val
-    x0 = np.real(val_0)
-    p0 = np.imag(val_0)
-    x1 = np.real(val_1)
-    p1 = np.imag(val_1)
-
-
+def calculate_cost_once(A, n_qmodes, bits):
     cost_value = 0
-
-    bit_0 = digitize_xp(x0)
-    bit_1 = digitize_xp(p0)
-    bit_2 = digitize_xp(x1)
-    bit_3 = digitize_xp(p1)
-    bits = [bit_0, bit_1, bit_2, bit_3]
-    
     for i in range(n_qmodes*2):
         for j in range(n_qmodes*2):
             if i==j:
                 continue
             if bits[i] != 0 and bits[j] != 0:
-                cost_value += A[i][j] * (bits[i] - bits[j])**2
+                cost_value += 0.25 * A[i][j] * (bits[i] - bits[j])**2
+    return cost_value
 
-    print(x0, p0, x1, p1)
-    # print(params)
+def circuit_with_cost_function(params):
+    n_qmodes = 2
+    c = 10
+
+    A = np.array([[c,1,0,0],
+                  [1,c,1,0],
+                  [0,1,c,1],
+                  [0,0,1,c]])
+
+    cost_value = 0
+    trials = 10
+    for i in range(trials):
+        bits = get_bits_from_circuit(A, n_qmodes, params)
+        cost_value += calculate_cost_once(A, n_qmodes, bits)
+    cost_value = -cost_value / trials
+
+    # print(x0, p0, x1, p1)
     log = {'Fitness': cost_value}
 
     # The second return value can be an optional log dictionary
@@ -105,7 +113,7 @@ def circuit_with_cost_function(params):
 
 
 def myloss(circuit_output):
-    return -circuit_output
+    return circuit_output
 
 
 # We have to define a regularizer function that penalises large parameters that we marked to be regularized
@@ -114,36 +122,63 @@ def myregularizer(regularized_params):
     # vector of all parameters
     return l2(regularized_params)
 
+def main():
+  # We add the regularizer function to the model
+  # The strength of regularizer is regulated by the
+  # hyperparameter 'regularization_strength'.
+  # Setting 'plot' to an integer automatically plots some default values
+  # as well as the monitored circuit parameters. (Requires matplotlib).
+  hyperparams = {'circuit': circuit_with_cost_function,
+                 'init_circuit_params': my_init_params,
+                 'task': 'optimization',
+                 'loss': myloss,
+                 'regularizer': myregularizer,
+                 'regularization_strength': 0.5,
+                 'optimizer': 'SGD',
+                 'init_learning_rate': 1e-7,
+                 'log_every': 1,
+                 'plot': True
+                 }
 
-# We add the regularizer function to the model
-# The strength of regularizer is regulated by the
-# hyperparameter 'regularization_strength'.
-# Setting 'plot' to an integer automatically plots some default values
-# as well as the monitored circuit parameters. (Requires matplotlib).
-hyperparams = {'circuit': circuit_with_cost_function,
-               'init_circuit_params': my_init_params,
-               'task': 'optimization',
-               'loss': myloss,
-               'regularizer': myregularizer,
-               'regularization_strength': 0.1,
-               'optimizer': 'SGD',
-               'init_learning_rate': 0.1,
-               'log_every': 1,
-               'plot': True
-               }
 
+  learner = CircuitLearner(hyperparams=hyperparams)
 
-learner = CircuitLearner(hyperparams=hyperparams)
+  learner.train_circuit(steps=100)
 
-learner.train_circuit(steps=10)
+  # Print out the final parameters
+  final_params = learner.get_circuit_parameters()
+  # final_params is a dictionary
+  for name, value in final_params.items():
+      print("Parameter {} has the final value {}.".format(name, value))
 
-# Print out the final parameters
-final_params = learner.get_circuit_parameters()
-# final_params is a dictionary
-for name, value in final_params.items():
-    print("Parameter {} has the final value {}.".format(name, value))
+  c = 10
+  A = np.array([[c,1,0,0],
+                [1,c,1,0],
+                [0,1,c,1],
+                [0,0,1,c]])
 
-# print(learner.run_circuit())
+  n_qmodes = 2
+  all_results = []
 
-# Look in the 'logsNUM' directory, there should be a file called 'log.csv' that records what happened to alpha
-# during training. Play around with the 'regularization_strength' and see how a large strength forces alpha to zero.
+  final_params_translated = []
+  final_params_translated.append(final_params["regularized/squeeze_0"])
+  final_params_translated.append(final_params["regularized/squeeze_1"])
+  final_params_translated.append(final_params["regularized/move_0"])
+  final_params_translated.append(final_params["regularized/move_1"])
+  final_params_translated.append(final_params["regularized/rotation_0"])
+  final_params_translated.append(final_params["regularized/rotation_1"])
+  
+  for i in range(1000):
+      bits = get_bits_from_circuit(A, n_qmodes, final_params_translated)
+      string_bits = [str(bit) for bit in bits]
+      all_results.append(",".join(string_bits))
+
+  print(Counter(all_results))
+
+  # print(learner.run_circuit())
+
+  # Look in the 'logsNUM' directory, there should be a file called 'log.csv' that records what happened to alpha
+  # during training. Play around with the 'regularization_strength' and see how a large strength forces alpha to zero.
+
+if __name__ == '__main__':
+    main()
